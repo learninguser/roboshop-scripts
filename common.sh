85cc6b962b3a6d1ad0d1d1e5be6edf47b22abc3c
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-01-06 23:32:00 +0100

    modified color codes for printing messages

--- common.sh
+++ common.sh
@@ -1,13 +1,13 @@
 status_check(){
     if[ $? -eq 0 ]; then
-        echo -e "\e[32mSuccess\e[0m"
+        echo -e "\e[1;32mSuccess\e[0m"
     else
-        echo -e "\e[31mFailure\e[0m"
+        echo -e "\e[1;31mFailure\e[0m"
         echo "Refer Log file ($1) for more information, "
         exit 1
     fi
 }
 
 print_message(){
-    echo -e "\e[1;35m$1\e[0m"
+    echo -e "\e[1m$1\e[0m"
 }

ce3462ce0bd1cf326cf31876db5d15b2cbc592f5
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-01-06 22:37:44 +0100

    added exit codes in case of failures

--- frontend.sh
+++ frontend.sh
@@ -5,45 +5,50 @@ current_dir=$(pwd)
 echo -e "\e[35mInstalling Nginx\e[0m"
 yum install nginx -y &>> $log_file
 if[ $? -eq 0 ]; then
-    echo "\e[32mSuccess\e[0m"
+    echo -e "\e[32mSuccess\e[0m"
 else
-    echo "\e[31mFailure\e[0m"
+    echo -e "\e[31mFailure\e[0m"
+exit 1
 fi
 
-echo "\e[35mRemoving existing files\e[0m"
+echo -e "\e[35mRemoving existing files\e[0m"
 rm -rf /usr/share/nginx/html/* &>> $log_file
 if[ $? -eq 0 ]; then
-    echo "\e[32mSuccess\e[0m"
+    echo -e "\e[32mSuccess\e[0m"
 else
-    echo "\e[31mFailure\e[0m"
+    echo -e "\e[31mFailure\e[0m"
+exit 1
 fi
 
-echo "\e[35mFetching frontend scrips\e[0m"
+echo -e "\e[35mFetching frontend scrips\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> $log_file
 if[ $? -eq 0 ]; then
-    echo "\e[32mSuccess\e[0m"
+    echo -e "\e[32mSuccess\e[0m"
 else
-    echo "\e[31mFailure\e[0m"
+    echo -e "\e[31mFailure\e[0m"
+exit 1
 fi
 
-echo "\e[35mExtracting frontend scripts\e[0m"
+echo -e "\e[35mExtracting frontend scripts\e[0m"
 cd /usr/share/nginx/html 
 unzip -o /tmp/frontend.zip
 if[ $? -eq 0 ]; then
-    echo "\e[32mSuccess\e[0m"
+    echo -e "\e[32mSuccess\e[0m"
 else
-    echo "\e[31mFailure\e[0m"
+    echo -e "\e[31mFailure\e[0m"
+exit 1
 fi
 
-echo "\e[35mCopying Roboshop conf to /etc/nginx/default.d location\e[0m"
+echo -e "\e[35mCopying Roboshop conf to /etc/nginx/default.d location\e[0m"
 cp $current_dir/files/roboshop.conf /etc/nginx/default.d/roboshop.conf
 if[ $? -eq 0 ]; then
-    echo "\e[32mSuccess\e[0m"
+    echo -e "\e[32mSuccess\e[0m"
 else
-    echo "\e[31mFailure\e[0m"
+    echo -e "\e[31mFailure\e[0m"
+exit 1
 fi
 
-echo "\e[35mRenaming each service with its Address\e[0m"
+echo -e "\e[35mRenaming each service with its Address\e[0m"
 sed -i -e "/catalogue/ s/localhost/catalogue-dev.learninguser.online/" /etc/nginx/default.d/roboshop.conf
 sed -i -e "/user/ s/localhost/user-dev.learninguser.online/" /etc/nginx/default.d/roboshop.conf
 sed -i -e "/cart/ s/localhost/cart-dev.learninguser.online/" /etc/nginx/default.d/roboshop.conf
@@ -51,16 +56,18 @@ sed -i -e "/shipping/ s/localhost/shipping-dev.learninguser.online/" /etc/nginx/
 sed -i -e "/payment/ s/localhost/payment-dev.learninguser.online/" /etc/nginx/default.d/roboshop.conf
 
 if[ $? -eq 0 ]; then
-    echo "\e[32mSuccess\e[0m"
+    echo -e "\e[32mSuccess\e[0m"
 else
-    echo "\e[31mFailure\e[0m"
+    echo -e "\e[31mFailure\e[0m"
+exit 1
 fi
 
-echo "\e[35mStarting nginx service\e[0m"
+echo -e "\e[35mStarting nginx service\e[0m"
 systemctl enable nginx &>> $log_file
 systemctl restart nginx &>> $log_file
 if[ $? -eq 0 ]; then
-    echo "\e[32mSuccess\e[0m"
+    echo -e "\e[32mSuccess\e[0m"
 else
-    echo "\e[31mFailure\e[0m"
+    echo -e "\e[31mFailure\e[0m"
+exit 1
 fi

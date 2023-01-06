1a1c3237b070051ccfa7a8284436175cb9586339
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-01-06 09:40:28 +0100

    added bash script for frontend

---
+++ frontend.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+log_file="/tmp/frontend.log"
+current_dir=$(pwd)
+
+echo "Installing Nginx"
+yum install nginx -y &>> $log_file
+
+echo "Removing existing files"
+rm -rf /usr/share/nginx/html/* &>> $log_file
+
+echo "Fetching frontend scrips"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> $log_file
+
+echo "Extracting frontend scripts"
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+
+echo "Copying Roboshop conf to /etc/nginx/default.d location"
+cp $current_dir/files/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo "Starting nginx service"
+systemctl enable nginx &>> $log_file
+systemctl start nginx &>> $log_file

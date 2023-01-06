e481c971d99b0f7ac08489210d2ac549dc003b9a
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-01-06 10:23:25 +0100

    added script for installing and running mongodb

---
+++ mongodb.sh
@@ -0,0 +1,13 @@
+log_file="/tmp/mongo.log"
+current_dir=$(pwd)
+
+echo "Installing MongoDB"
+cp $current_dir/files/mongo.repo /etc/yum.repos.d/mongo.repo &>> $log_file
+yum install mongodb-org -y &>> $log_file
+
+echo "Updating listen address from 127.0.0.1 to 0.0.0.0"
+sed -i "s/127.0.0.1/0.0.0.0" /etc/mongo.conf
+
+echo "Starting MongoDB service"
+systemctl enable mongod &>> $log_file
+systemctl start mongod &>> $log_file

177e9fc3af62d4efb4cb44d0681e2d0dd7e2e2fc
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-01-06 22:08:37 +0100

    renaming all services in conf file with records

--- frontend.sh
+++ frontend.sh
@@ -43,6 +43,19 @@ else
     echo "\e[31mFailure\e[0m"
 fi
 
+echo "\e[35mRenaming each service with its Address\e[0m"
+sed -i -e "/catalogue/ s/localhost/catalogue-dev.learninguser.online/" /etc/nginx/default.d/roboshop.conf
+sed -i -e "/user/ s/localhost/user-dev.learninguser.online/" /etc/nginx/default.d/roboshop.conf
+sed -i -e "/cart/ s/localhost/cart-dev.learninguser.online/" /etc/nginx/default.d/roboshop.conf
+sed -i -e "/shipping/ s/localhost/shipping-dev.learninguser.online/" /etc/nginx/default.d/roboshop.conf
+sed -i -e "/payment/ s/localhost/payment-dev.learninguser.online/" /etc/nginx/default.d/roboshop.conf
+
+if[ $? -eq 0 ]; then
+    echo "\e[32mSuccess\e[0m"
+else
+    echo "\e[31mFailure\e[0m"
+fi
+
 echo "\e[35mStarting nginx service\e[0m"
 systemctl enable nginx &>> $log_file
 systemctl restart nginx &>> $log_file

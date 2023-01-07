455ea29367dc83faa86c4a4f02cedaa8ecf0d6b3
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-01-07 22:09:43 +0100

    redirecting output to log file in MySQL

--- mysql.sh
+++ mysql.sh
@@ -9,26 +9,26 @@ if [ -z $mysql_root_password ]; then
 fi
 
 print_message "Disabling default MySQL version and enabling v5.7"
-dnf module disable mysql -y
+dnf module disable mysql -y &>> $log_file
 status_check
 
 print_message "Copy repo file"
-cp $current_dir/files/mysql.repo /etc/yum.repos.d/mysql.repo
+cp $current_dir/files/mysql.repo /etc/yum.repos.d/mysql.repo &>> $log_file
 status_check
 
 print_message "Installing MySQL server"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>> $log_file
 status_check
 
 print_message "Start MySQL service"
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>> $log_file
+systemctl start mysqld &>> $log_file
 status_check
 
 print_message "Changing default MySQL Root password"
-mysql_secure_installation --set-root-pass $mysql_root_password
+mysql_secure_installation --set-root-pass $mysql_root_password &>> $log_file
 status_check
 
 print_message "Checking if the set password is working or not"
-mysql -uroot -p$mysql_root_password
+mysql -uroot -p$mysql_root_password &>> $log_file
 status_check

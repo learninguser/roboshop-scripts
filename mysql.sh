554020be3d43b350eb5fa4c46aab9a8772683cde
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-01-09 17:03:12 +0100

    checking if mysql root password is already changed or not

--- mysql.sh
+++ mysql.sh
@@ -26,7 +26,7 @@ systemctl start mysqld &>> $log_file
 status_check $log_file
 
 print_message "Changing default MySQL Root password"
-mysql -uroot -p$mysql_root_password &>> $log_file
+echo "show databases" | mysql -uroot -p$mysql_root_password &>> $log_file
 if [ $? -ne 0 ]; then
   mysql_secure_installation --set-root-pass $mysql_root_password &>> $log_file
 fi
